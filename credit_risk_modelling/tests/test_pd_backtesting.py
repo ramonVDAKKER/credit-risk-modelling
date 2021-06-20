e45c39b9fbd095fb579fa71f234b764a46908670
Ramon van den Akker <ramon.vdakker@gmail.com>
2021-06-20 20:41:22 +0200

    add dummy test

--- credit_risk_modelling/tests/test_pd_backtesting.py
+++ credit_risk_modelling/tests/test_pd_backtesting.py
@@ -2,3 +2,15 @@
 
 import pytest
 import credit_risk_modelling.pd_backtesting as back
+import numpy as np
+
+
+class TestDummy:
+    """Dummy."""
+
+    def test1(self):
+        """Test dummy."""
+        a = 1
+        b = 1.0
+        np.testing.assert_almost_equal(a, b)
+
